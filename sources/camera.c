5696db0cfa6d3d3812f4064bf7a5c9b03f90ee9f
Jana Jovicic <jana.jovicic755@gmail.com>
2018-04-04 09:50:49 +0200

    obrada kolizije sa policama

--- sources/camera.c
+++ sources/camera.c
@@ -1,5 +1,14 @@
+/*
+Kretanje kamere sam napravila na osnovu koda sa sledeceg linka:
+http://www.lighthouse3d.com/tutorials/glut-tutorial/keyboard-example-moving-around-the-world/
+
+Odnosno, odatle sam iskoristila formule za kretanje i ideju kako da ih povezem sa funkcijama iz keyboard.c.
+Obradu kolizija na ovako glup nacin sam sama radila. Prvo sam pokusala nekako bolje da uradim, ali onda se sve poremetilo i u nekim pravcima uopste nije moglo da se krece. Zato sam ostavila ovako.
+*/
+        
 #include "../headers/global_variables.h"
 #include "../headers/camera.h"
+#include "../headers/bookcase.h"
 
 void resetCamera()
 {
@@ -39,36 +48,119 @@ void updateCamera()
 	else if (gCameraYaw < 0.0f)
 		gCameraYaw += 360.0f;
 
-        /*
-            Kretanje kamere sam napravila na osnovu koda sa sledeceg linka:
-            http://www.lighthouse3d.com/tutorials/glut-tutorial/keyboard-example-moving-around-the-world/
-        */
         
-        // desno
+        /* Obrada kolizije sa prednjom desnom policom za knjige (koja je druga u nizu bookcases[]) */
+        if(gCameraPosition[0] > 0 && gCameraPosition[2] > 0)
+        {
+            /* ako smo sa prednje strane ormana */
+            if(gCameraPosition[2] > bookcases[2].z)
+            {
+                if(gCameraPosition[2] < bookcases[2].z + 20 && gCameraPosition[0] > bookcases[2].x - 65)
+                    gCameraPosition[2] = bookcases[2].z + 20;
+                else
+                    gCameraPosition[2] -= cos(DEG2RAD(gCameraYaw)) * (gMoveForward * 2);
+            }
+            /* ako smo sa zadnje strane ormana */
+            else if(gCameraPosition[2] < bookcases[2].z) 
+            {
+                if(gCameraPosition[2] > bookcases[2].z - 20  && gCameraPosition[0] > bookcases[2].x - 65)
+                    gCameraPosition[2] = bookcases[2].z - 20;
+                else
+                    gCameraPosition[2] += -cos(DEG2RAD(gCameraYaw + 90)) * (gMoveRightward * 2);
+            }
+        }
+        /* Obrada kolizije sa prednjom levom policom za knjige (koja je treca u nizu bookcases[]) */
+        else if(gCameraPosition[0] < 0 && gCameraPosition[2] > 0)
+        {
+            /* ako smo sa prednje strane ormana */
+            if(gCameraPosition[2] > bookcases[3].z)
+            {
+                if(gCameraPosition[2] < bookcases[3].z + 20 && gCameraPosition[0] < bookcases[3].x + 65)
+                    gCameraPosition[2] = bookcases[3].z + 20;
+                else
+                    gCameraPosition[2] -= cos(DEG2RAD(gCameraYaw)) * (gMoveForward * 2);
+            }
+            /* ako smo sa zadnje strane ormana */
+            else if(gCameraPosition[2] < bookcases[2].z) 
+            {
+                if(gCameraPosition[2] > bookcases[3].z - 20  && gCameraPosition[0] < bookcases[3].x + 65)
+                    gCameraPosition[2] = bookcases[3].z - 20;
+                else
+                    gCameraPosition[2] += -cos(DEG2RAD(gCameraYaw + 90)) * (gMoveRightward * 2);
+            }
+        }
+        /* Obrada kolizije sa zadnjom desnom policom za knjige (koja je nulta u nizu bookcases[]) */
+        else if(gCameraPosition[0] > 0 && gCameraPosition[2] < 0)
+        {
+            /* ako smo sa prednje strane ormana */
+            if(gCameraPosition[2] > bookcases[0].z)
+            {
+                if(gCameraPosition[2] < bookcases[0].z + 20 && gCameraPosition[0] > bookcases[0].x - 65)
+                    gCameraPosition[2] = bookcases[0].z + 20;
+                else
+                    gCameraPosition[2] -= cos(DEG2RAD(gCameraYaw)) * (gMoveForward * 2);
+            }
+            /* ako smo sa zadnje strane ormana */
+            else if(gCameraPosition[2] < bookcases[2].z) 
+            {
+                if(gCameraPosition[2] > bookcases[0].z - 20  && gCameraPosition[0] > bookcases[0].x - 65)
+                    gCameraPosition[2] = bookcases[0].z - 20;
+                else
+                    gCameraPosition[2] += -cos(DEG2RAD(gCameraYaw + 90)) * (gMoveRightward * 2);
+            }
+        }
+        /* Obrada kolizije sa zadnjom levom policom za knjige (koja je prva u nizu bookcases[]) i stolom */
+        else if(gCameraPosition[0] < 0 && gCameraPosition[2] < 0)
+        {
+            /* ako smo sa prednje strane ormana */
+            if(gCameraPosition[2] > bookcases[1].z)
+            {
+                if(gCameraPosition[2] < bookcases[1].z + 20 && gCameraPosition[0] < bookcases[1].x + 65)
+                    gCameraPosition[2] = bookcases[1].z + 20;
+                else
+                    gCameraPosition[2] -= cos(DEG2RAD(gCameraYaw)) * (gMoveForward * 2);
+            }
+            /* ako smo sa zadnje strane ormana */
+            else if(gCameraPosition[2] < bookcases[2].z) 
+            {
+                if(gCameraPosition[2] > bookcases[1].z - 20  && gCameraPosition[0] < bookcases[1].x + 65)
+                    gCameraPosition[2] = bookcases[1].z - 20;
+                else
+                    gCameraPosition[2] += -cos(DEG2RAD(gCameraYaw + 90)) * (gMoveRightward * 2);
+                /* kolizija sa stolom */ 
+                if(gCameraPosition[0] < -80)
+                    gCameraPosition[0] = -80;
+            }
+        }
+        
+        /* Obrada kolizija sa zidovima */
+        
+        // desni zid
         if (gCameraPosition[0] > xRange - 5)
             gCameraPosition[0] = xRange - 5;
         else
             gCameraPosition[0] += sin(DEG2RAD(gCameraYaw)) * (gMoveForward * 2);
        
-        // pravo
+        // prednji zid
         if (gCameraPosition[2] < -zRange + 5)
             gCameraPosition[2] = -zRange + 5;
         else
             gCameraPosition[2] -= cos(DEG2RAD(gCameraYaw)) * (gMoveForward * 2);
 
-        // levo
+        // levi zid
         if (gCameraPosition[0] < -xRange + 5)
             gCameraPosition[0] = -xRange + 5;
         else
             gCameraPosition[0] += sin(DEG2RAD(gCameraYaw + 90)) * (gMoveRightward * 2);
         
-        // nazad
+        // zadnji zid
         if (gCameraPosition[2] > zRange - 5)
             gCameraPosition[2] = zRange - 5;
         else
             gCameraPosition[2] += -cos(DEG2RAD(gCameraYaw + 90)) * (gMoveRightward * 2);
 
 	gCameraPosition[1] += gMoveUpward;
+        
 
 	gCameraLookAt[0] = gCameraPosition[0] + sin(DEG2RAD(gCameraYaw));
 	gCameraLookAt[2] = gCameraPosition[2] - cos(DEG2RAD(gCameraYaw));
