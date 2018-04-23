e08b7c6990210b22f1eddf5979e0b465ef0995a8
Jana Jovicic <jana.jovicic755@gmail.com>
2018-04-23 20:14:31 +0200

    omoguceno otvaranje novog prozora

--- sources/keyboard.c
+++ sources/keyboard.c
@@ -1,6 +1,14 @@
 #include "../headers/global_variables.h"
 #include "../headers/keyboard.h"
 #include "../headers/camera.h"
+#include "../headers/bookcase.h"
+
+extern GLFWwindow* bookWindow;
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
 
 void keyboardFunc(unsigned char key, GLint x, GLint y)
 {
@@ -20,6 +28,35 @@ void keyboardFunc(unsigned char key, GLint x, GLint y)
 
 		case 'r':	resetCamera();
 					break;
+                                        
+                case 'o':       if(gCameraPosition[0] > books[0].x+27 && gCameraPosition[0] < books[0].x+33
+                                    && gCameraPosition[2] > books[0].z-26 &&  gCameraPosition[2] < books[0].z+13){
+                                    // printf("!!!!!\n");
+                                        
+                                        glfwSetErrorCallback(error_callback);
+
+                                        if (!glfwInit()) {
+                                            exit(EXIT_FAILURE);
+                                        }
+                                        
+                                        bookWindow = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "George Orwell, 1984", NULL, NULL);
+                                        
+                                        if(!bookWindow){
+                                            glfwTerminate();
+                                            exit(EXIT_FAILURE);
+                                        }
+                                        
+                                        while(!glfwWindowShouldClose(bookWindow)){
+                                            displayBook(books[0].title);
+                                            glfwPollEvents();
+                                        }
+
+                                        glfwDestroyWindow(bookWindow);
+                                        glfwTerminate();
+                                }
+                                
+                
+
 	}
 	updateCamera();
 }
